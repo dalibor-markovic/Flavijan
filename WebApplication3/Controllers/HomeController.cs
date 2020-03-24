f5b42c5b974165a900db7c46003304eb468c4dca
Dalibor <studioflavijan@gmail.com>
2020-03-24 13:29:45 +0100

    Ostalo jos izuzeci - provera unosa

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -21,68 +21,61 @@ namespace WebApplication3.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Opis sajta";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Kontakt stranica";
+
+            return View();
+        }
+
+        public ActionResult Tabela()
+        {
+            ViewBag.Message = "Tabela zaposlenih";
 
             return View();
         }
 
         [HttpPost]        
-        public ContentResult saveObj(Radnik radnik)
+        public ContentResult Upisi(Radnik radnik)
         {
+            Radnik.connectionString = "Server=DESKTOP-4CRF79E;Database=Gemini;Trusted_Connection=True;ConnectRetryCount=3;";
 
-            //            Radnik radnik = new Radnik();
-            //            Radnik radnik = JsonConvert.DeserializeObject<Radnik>(ime);
-            //           Radnik radnik = new Radnik();
-            //            radnik = JsonConvert.DeserializeObject<Radnik>(json);
-            //          string izlaz = JsonConvert.SerializeObject(radnik);
+            return Content(JsonConvert.SerializeObject(Radnik.UpisiNovogRadnika(radnik)));
 
-            return Content(JsonConvert.SerializeObject(radnik));
-
-            /*
-                        string connectionString = "Server=DESKTOP-4CRF79E;Database=Gemini;Trusted_Connection=True;ConnectRetryCount=3;";
-
-                        //
-                        // In a using statement, acquire the SqlConnection as a resource.
-                        //
-                        List<myobjFilter> lstZaposleni = new List<myobjFilter>();
-
-
-                                   using (SqlConnection con = new SqlConnection(connectionString))
-                                   {
-                                       //
-                                       // Open the SqlConnection.
-                                       //
-                                       con.Open();
-                                       //
-                                       // This code uses an SqlCommand based on the SqlConnection.
-                                       //
-                                       using (SqlCommand command = new SqlCommand("SELECT TOP 2 * FROM Zaposleni", con))
-                                       using (SqlDataReader reader = command.ExecuteReader())
-                                       {
-                                           while (reader.Read())
-                                           {
-                                               Console.WriteLine("{0} {1} {2}",
-                                                   reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
-
-                                           }
-                                       }
-                                   }
-
-                                   var myobj = new myobjFilter();
-                        lstZaposleni.Add(myobj);
-
-                                   return Content(JsonConvert.SerializeObject(lstZaposleni));
-
-            */
         }
 
+        [HttpPost]
+        public ContentResult Vrati(Radnik radnik)
+        {
+            Radnik.connectionString = "Server=DESKTOP-4CRF79E;Database=Gemini;Trusted_Connection=True;ConnectRetryCount=3;";
+
+            return Content(JsonConvert.SerializeObject(Radnik.IzveziTabelu()));
+        }
+
+        [HttpPost]
+        public ContentResult Obrisi(Radnik radnik)
+        {
+            Radnik.connectionString = "Server=DESKTOP-4CRF79E;Database=Gemini;Trusted_Connection=True;ConnectRetryCount=3;";
+
+            Radnik.ObrisiRadnika(radnik.ID);
+
+            return Content(JsonConvert.SerializeObject(radnik));
+        }
+
+        [HttpPost]
+        public ContentResult Izmeni(Radnik radnik)
+        {
+            Radnik.connectionString = "Server=DESKTOP-4CRF79E;Database=Gemini;Trusted_Connection=True;ConnectRetryCount=3;";
+
+            Radnik.PromeniRadnika(radnik);
+
+            return Content(JsonConvert.SerializeObject(radnik));
+        }
 
 
     }
--- WebApplication3/Models/Radnik.cs
+++ WebApplication3/Models/Radnik.cs
@@ -2,14 +2,101 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace WebApplication3.Models
 {
     public class Radnik
     {
+
+        public int ID { get; set; }
         public string ime { get; set; }
         public string prezime { get; set; }
         public string adresa { get; set; }
         public float netoPlata { get; set; }
+
+        public float brutoPlata { get; set; }
+
+        public static string connectionString { get; set; }
+        
+        public static Radnik UpisiNovogRadnika(Radnik radnik)
+        {
+            radnik.brutoPlata = (float)(radnik.netoPlata + (radnik.netoPlata * 36.55 / 100));
+
+                using (SqlConnection konekcija = new SqlConnection(Radnik.connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand("INSERT INTO Zaposleni VALUES(@Ime, @Prezime, @Adresa, @Neto_plata, @Bruto_plata)", konekcija))
+                    {
+                        command.Parameters.AddWithValue("@Ime", radnik.ime);
+                        command.Parameters.AddWithValue("@Prezime", radnik.prezime);
+                        command.Parameters.AddWithValue("@Adresa", radnik.adresa);
+                        command.Parameters.AddWithValue("@Neto_plata", radnik.netoPlata);
+                        command.Parameters.AddWithValue("@Bruto_plata", radnik.brutoPlata);
+
+                        konekcija.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+            return radnik;
+        }
+
+        public static Boolean PromeniRadnika(Radnik radnik) {
+
+            radnik.brutoPlata = (float)(radnik.netoPlata + (radnik.netoPlata * 36.55 / 100));
+
+            using (SqlConnection konekcija = new SqlConnection(Radnik.connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("UPDATE Zaposleni "+
+                    "SET Ime = '"+radnik.ime+"', Prezime = '"+radnik.prezime+
+                    "', Adresa = '"+radnik.adresa+"', Neto_plata = '"+radnik.netoPlata+
+                    "', Bruto_plata = '"+radnik.brutoPlata+"' WHERE ID = "+radnik.ID, konekcija))
+                {
+                    konekcija.Open();
+                    command.ExecuteNonQuery();
+                }
+
+            }
+
+            return true;
+        }
+
+        public static Boolean ObrisiRadnika(int id) {
+            using (SqlConnection konekcija = new SqlConnection(Radnik.connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("DELETE FROM Zaposleni WHERE ID="+id, konekcija))
+                {
+                    konekcija.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            return true;
+        }
+
+        public static List<Radnik> IzveziTabelu() {
+            List<Radnik> listaRadnika = new List<Radnik>();
+            using (SqlConnection konekcija = new SqlConnection(Radnik.connectionString)) {
+                konekcija.Open();
+
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Zaposleni", konekcija))
+                using (SqlDataReader citac = command.ExecuteReader())
+                {
+                    while (citac.Read())
+                    {
+                        Radnik radnik = new Radnik();
+                        radnik.ID = citac.GetInt32(0);
+                        radnik.ime = citac.GetString(1);
+                        radnik.prezime = citac.GetString(2);
+                        radnik.adresa = citac.GetString(3);
+                        radnik.netoPlata = citac.GetFloat(4); 
+                        radnik.brutoPlata = citac.GetFloat(5);
+                        listaRadnika.Add(radnik);
+                    }
+                }
+            }
+
+            return listaRadnika;
+        }
     }
 }
